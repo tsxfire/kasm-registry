ddd552f2a8302dbaea5b80c3584a51508bbeb648
tsxfire <32419215+tsxfire@users.noreply.github.com>
2025-02-03 16:04:51 -0600

    test1

--- site/next.config.js
+++ site/next.config.js
@@ -3,7 +3,7 @@
 const nextConfig = {
   env: {
     name: 'NerdsWork Registry',
-    description: 'The NerdsWork Kasm supported workspaces.',
+    description: 'The NerdsWork Kasm workspaces.',
     icon: 'https://tsxfire.github.io/kasm-registry/1.1/NerdsworkLogo.png',
     listUrl: 'https://tsxfire.github.io/kasm-registry/',
     contactUrl: 'https://github.com/tsxfire/kasm-registry/issues',
